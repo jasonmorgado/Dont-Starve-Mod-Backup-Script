e01b6dd6d46e447af8759730c401c47c66beb145
domo106 <jasonsanimations@gmail.com>
2020-12-22 22:49:20 -0500

    Added support for modinfo files with latin-1 encoding
    Useful for translation mods that use non UTF-8 characters.

--- backup_mods.py
+++ backup_mods.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import sys
+import time
 
 # Set these manually if not using command line
 MODS_FOLDER_PATH = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Don't Starve Together\\mods"
@@ -59,12 +60,19 @@ def backup_mods(mods_folder_path, backup_path):
         # Get mod name
         mod_name = ""
         workshop_id = folder_name.split("-")[1]
-        with open(modinfo_path, 'r', encoding="utf8") as modinfo_file:
-            for line in modinfo_file:
-                if line[:4] == "name":
-                    mod_name = line.split('=')[1].strip(" \n")
-                    break
-        
+        try:
+            with open(modinfo_path, 'r', encoding='utf-8') as modinfo_file:
+                for line in modinfo_file:
+                    if line[:4] == "name":
+                        mod_name = line.split('=')[1].strip(" \n")
+                        break
+        except UnicodeDecodeError:
+            print("Unexpected encoding, trying another way...")
+            with open(modinfo_path, 'r', encoding='latin-1') as modinfo_file:
+                for line in modinfo_file:
+                    if line[:4] == "name":
+                        mod_name = line.split('=')[1].strip(" \n")
+                        break
         # Copy to dest
         move_folder(mod_path, backup_path, mod_name)
     print("Done!")
@@ -77,3 +85,4 @@ if __name__ == '__main__':
         backup_mods(sys.argv[1], sys.argv[2])
     else:
         print("Got {} command line arguments, expected 2.".format(len(sys.argv)-1))
+    time.sleep(10)
